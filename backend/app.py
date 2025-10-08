2335a2a94a0e23a214ab84061cbe4c9954bbef7d
oyasuminaaa <150317826+oyasuminaaa@users.noreply.github.com>
2025-10-08 23:38:42 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,10 @@ import requests
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-GEMINI_API_KEY = "AIzaSyB4niyedJwvvofav1ZaUHIwLrbclc3bPPA" 
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("No GEMINI_API_KEY set for Flask application")
+API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{GEMINI_MODEL_NAME}:generateContent?key={GEMINI_API_KEY}"
 GEMINI_MODEL_NAME = "gemini-2.5-flash-preview-05-20"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{GEMINI_MODEL_NAME}:generateContent?key={GEMINI_API_KEY}"
 
